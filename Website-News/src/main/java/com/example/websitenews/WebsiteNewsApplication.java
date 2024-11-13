fbbfc1c1a35db3d2e85d0803ab5db148142703d9
apostoloszz <136325513+apostoloszz@users.noreply.github.com>
2024-11-13 19:45:14 +0700

    initial commit

---
+++ Website-News/src/main/java/com/example/websitenews/WebsiteNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.websitenews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteNewsApplication.class, args);
+	}
+
+}
